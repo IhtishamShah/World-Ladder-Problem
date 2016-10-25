c848574e50cdc110070a6bbd0d6eaf95ab99c113
IhtishamShah <ihtisham.shah48@gmail.com>
2016-10-26 00:03:23 +0500

    BFS for finding shortest path

--- task1.py
+++ task1.py
@@ -1,4 +1,7 @@
 import json
+import networkx as nx
+import distance
+
 
 # find all paths between starting and ending point; append them to an array
 def findAllPaths(graph, start, end, path=[]):
@@ -32,9 +35,9 @@ def maxPath(totalPaths):
 	return totalPaths[length.index(max(length))]
 
 # find total number of words of same length in dictionary 
-def calculateWordsLength(dictionary):
+def calculateWordsLength(wordDictionary):
 	wordLength = {}
-	for keys in dictionary:
+	for keys in wordDictionary:
 		if not len(keys) in wordLength.keys():
 			wordLength[len(keys)] = 1
 		else:
@@ -42,11 +45,11 @@ def calculateWordsLength(dictionary):
 	return wordLength
 
 # claculate the starting index based on length of starting point
-def startingIndex(wordLength, startingIndex):
+def startingIndex(wordLengthDictionary, word):
 	index = 0
-	for key in wordLength.keys():
-		if key < len(starting):
-			index+= wordLength[key]
+	for key in wordLengthDictionary.keys():
+		if key < len(word):
+			index+= wordLengthDictionary[key]
 	return index
 
 def stringToAscii(word):
@@ -59,33 +62,93 @@ def asciiToString(word):
 # def populateGraph(starting, ending, dictionary):
 # 	if  not len(startingState) == len(endingState):
 
-def getSuccessors(dictionary, word, startingIndex, endingIndex):
-
+def getSuccessors(wordDictionary, word,target, startingIndex, endingIndex, chain, frontier):
+	chain[word] =[]
 	wordAscii = stringToAscii(word)
-	child = []
-	for letter in range(0, len(wordAscii)):
-		# print wordAscii[letter]
-		# temp = wordAscii
-		temp = []
-		for change in range(65,91):
-			tempAscii = list(wordAscii)
-			tempAscii[letter] = change
-			# print asciiToString(temp)
-			tempString = asciiToString(tempAscii)
-			if tempString in dictionary[startingIndex:startingIndex+endingIndex]:
-				child.append(tempString)
+	# child = []
+	# for letter in range(0, len(wordAscii)):
+	# 	# print wordAscii[letter]
+	# 	# temp = wordAscii
+	# 	temp = []
+	tempString = word
+	# print chain
+
+	for key in wordDictionary[startingIndex:startingIndex+endingIndex]:
+	# for change in range(65,91):
+		# tempIter = letter
+		# tempAscii = list(wordAscii)
+		# tempAscii[letter] = change
+		# # print asciiToString(temp)
+		# tempString = asciiToString(tempAscii)
+		if distance.hamming(tempString,key) == 1:
+			# print "woooo"
+		# if tempString in wordDictionary[startingIndex:startingIndex+endingIndex]:
+			if not key in chain.keys():
+				# print "vat"
+				frontier.append(key)
+				chain[word].append(key)
+				if tempString == target:
+					print "Woah"
+					return
+			# if tempIter == 90:
+			# 	tempIter = 65
 			# print "word",wordAscii
 		# temp = wordAscii
+	if len(chain[word]) == 0:
+		chain.pop(word, None)
+	# chain[word]=child
 
-	return child
+def findChain(wordDictionary, word,target, startingIndex, endingIndex, chain, frontier):
+	# print word
+	# if word == target:
+	# 	return
+	
+
+	# getSuccessors(wordDictionary,word,target, startingIndex, endingIndex, chain,frontier)
+	# print chain
+	# print frontier
+	# findChain(wordDictionary, word,target, startingIndex, endingIndex, chain, frontier)
+	while frontier:
+		word = frontier.pop()
+		# print word
+
+		if(word==target):
+			return
+		getSuccessors(wordDictionary,word,target, startingIndex, endingIndex, chain,frontier)
 
 
+	# for item in chain[word]:
+	# 	childWord = frontier.pop()
+	# 	if childWord == target:
+	# 		print "found it"
+	# 	getSuccessors(wordDictionary,childWord,target, startingIndex, endingIndex, chain,frontier)
 
+
+		
 # parse json file and return the keys of the json object
 def parseJson(fileName):
 	f=open(fileName)
 	jsonDictionary = json.loads(f.read())
 	return jsonDictionary.keys()
+
+def bfs(graph, start, end):
+    # maintain a queue of paths
+    queue = []
+    # push the first path into the queue
+    queue.append([start])
+    while queue:
+        # get the first path from the queue
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        # path found
+        if node == end:
+            return path
+        # enumerate all adjacent nodes, construct a new path and push it into the queue
+        for adjacent in graph.get(node, []):
+            new_path = list(path)
+            new_path.append(adjacent)
+            queue.append(new_path)
 
 graph = {'Apple': ['abple', 'bhdfs'],
     'B': ['C', 'D'],
@@ -101,28 +164,57 @@ graph = {'Apple': ['abple', 'bhdfs'],
 
 
 dictionary = parseJson('dictionary.json')
+dictionary.sort() 
 dictionary.sort(lambda x,y: cmp(len(x), len(y)))
+# dictionary.sort() # sorts normally by alphabetical order
+# dictionary.sort(key=len, reverse=True)
 # print dictionary
 iterator = 0
 
-# for keys in dictionary:
-# 	if(iterator==1000):
-# 		break
-# 	print keys
-# 	iterator+=1
-
-
 wordLength = calculateWordsLength(dictionary)
-print wordLength
-starting  = "HEAT"
+# print wordLength
+starting  = "CUTE"
+ending ="LOOT"
 endIndex = wordLength[len(starting)]
 
 # iterator = 0
 # # startIndex = wordLength[len(starting)-1]
 
-index = startingIndex(wordLength,endIndex)
+index = startingIndex(wordLength,starting)
 print index
 print endIndex
+
+wordChain = {}
+frontier =[]
+frontier.append(starting)
+findChain(dictionary, starting,ending, index, endIndex, wordChain, frontier)
+
+
+
+# for key in dictionary[wordLength[1]+wordLength[2]:wordLength[3]+wordLength[1]+wordLength[2]]:
+# 	print key
+# print findAllPaths(wordChain,starting,ending)
+# for key in wordChain.keys():
+# 	print wordChain[key]
+# wordGraph = nx.DiGraph()
+
+# for key in wordChain.keys():
+# 	for item in wordChain[key]:
+# 		wordGraph.add_edge(key,item)
+print bfs(wordChain, starting,ending)
+# print (nx.algorithms.bfs_tree(wordGraph, starting).edges())
+
+
+# print  [0].extend(nx.algorithms.bfs_successors(wordGraph, starting).values())
+
+# pathsToTarget = []
+
+# for path in nx.all_simple_paths(wordGraph, source=starting, target=ending):  
+# 	len(path)
+#     pathsToTarget.append(path)
+# print minPath(pathsToTarget)
+# print nx.shortest_path(wordGraph,source=starting,target=ending)
+# print minPath(findAllPaths(wordChain,starting,ending))
 
 # for key in dictionary[index:index+endIndex]:
 # 	print key
@@ -130,34 +222,10 @@ print endIndex
 # print "total",iterator
 
 
-
-#find chain
-wordChain = {}
-# 
-# print wordAscii
-# word = asciiToString(wordAscii)
-# print word
-
-# Ascii -> A= 65 Z= 90
-	# wordAscii = stringToAscii(starting)
-	# print wordAscii
-	# child = []
-	# for letter in range(0, len(wordAscii)):
-	# 	# print wordAscii[letter]
-	# 	# temp = wordAscii
-	# 	temp = []
-	# 	for change in range(65,91):
-	# 		tempAscii = list(wordAscii)
-	# 		tempAscii[letter] = change
-	# 		# print asciiToString(temp)
-	# 		tempString = asciiToString(tempAscii)
-	# 		if tempString in dictionary[index:index+endIndex]:
-	# 			child.append(tempString)
-	# 		# print "word",wordAscii
-	# 	# temp = wordAscii
-print getSuccessors(dictionary,starting, index, endIndex)
+# print getSuccessors(dictionary,starting, index, endIndex)
 	
 		
+
 
 
 # dictionary[index:index+endIndex]
@@ -165,3 +233,7 @@ print getSuccessors(dictionary,starting, index, endIndex)
 # print(maxPath(findAllPaths(graph,'A','D')))
 
 
+
+
+
+print distance.hamming(starting,ending)
